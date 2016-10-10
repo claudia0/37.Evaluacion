4f7104fce243400b95af9accdbaca9f66ff29667
claudia0 <reyesclaudiav@gmail.com>
2016-10-10 13:16:44 -0500

    Primer intento

---
+++ EvaluaciónG3/ejercicio1.js
@@ -0,0 +1,22 @@
+// <h4>Ejercicio 1: <br>Haz el juego de piedra papel o tijera con 3 turnos</h4
+
+// PSEUDOCODIGO
+// 1.INICIO
+// 2.Pide al usuario que elija piedra, papel o tijera durante 3 turnos
+// 3. si elige 1 el jugador
+
+
+
+
+while (var turno <= 3){
+	var datos = prompt("Elige 1=Piedra, 2=Papel o 3=Tijera");
+	if ( datos = 1){
+      alert("papel gana");
+	}
+	else if (datos = 2){
+		alert("Tijera Pierde");
+	}
+	else(datos= 3){
+		alert("Piedra gana");
+	}
+}
---
+++ EvaluaciónG3/ejercicio2.js
@@ -0,0 +1,24 @@
+// <h4>Ejercicio 2:Declara un arreglo vacío denominado nombres. 
+// Pide al usuario tres nombres y almacena esos nombres como elementos arreglo.
+ // A continuación muestra el contenido en un mensaje.</h4>
+
+// PSEUDOCÓDIGO
+
+// INICIO
+// 1.pide al usuario que ingrese el primer nombre
+// 2.lee nombres
+// 3.pide al usuario que ingrese el segundo nombre
+// 4.lee nombres
+// 5.pide al usuario que ingrese el tercer nombre
+// 6.lee nombres
+// 7.sumar nombres y guardarlos en var suma
+// 8.imprime suma
+// 9.FIN
+
+		var nombres[];
+		if (nombres<=3){
+			
+			var datos = prompt("Ingrese nombre");
+			var suma = suma + datos;
+			console.log(suma);	
+	}
